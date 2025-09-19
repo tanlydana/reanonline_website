7ed1f93fd9874507abf53eb0c079b575f9c67228
dana <lydana32@gmail.com>
2025-09-19 23:36:58 +0700

    fix: update vite.config.ts to resolve __dirname and path errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname,resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
